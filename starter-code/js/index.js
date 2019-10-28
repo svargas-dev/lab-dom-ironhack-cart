4a9849e3b5ce7276b263ad623443fcc6a309bb85
Sam Vargas <sam.vargas@gmx.com>
2019-10-28 15:11:36 +0000

    Iteration4 complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,34 +1,53 @@
 // ITERATION 1
 
-function updateSubtotal($product) {
+function updateSubtotal($product, productIndex) {
   let $priceObj = document.querySelectorAll('.price > span');
   let $quantityObj = document.querySelectorAll('.quantity > input');
   let $subtotalObj = document.querySelectorAll('.subtotal > span');
   
-  let $price = Number($priceObj[0].textContent);
-  let $quantity = + Number($quantityObj[0].valueAsNumber);
+  let $price = Number($priceObj[productIndex].textContent);
+  let $quantity = + Number($quantityObj[productIndex].valueAsNumber);
   // console.log('price ' + $price);
   // console.log('quanity ' + $quantity);
-  $subtotalObj[0].innerText = ($price * $quantity).toFixed(2);
+  $subtotalObj[productIndex].innerText = ($price * $quantity).toFixed(2);
+  return $price * $quantity;
 }
-
 
 
 function calculateAll() {
   // ITERATION 2
-  //...
+  // let $products = document.querySelectorAll('.product');
+  // let total = 0;
+  // for (let productIndex=0; productIndex < $products.length; productIndex++) {
+  //   updateSubtotal($products[productIndex], productIndex);
+  // }
+
   // ITERATION 3
-  //...
+  // sum the product subtotals
+
+  let $products = document.querySelectorAll('.product');
+  let total = 0;
+  for (let productIndex=0; productIndex < $products.length; productIndex++) {
+    total += updateSubtotal($products[productIndex], productIndex);
+  }
+  document.querySelector('#total-value > span').innerText = total.toFixed(2);
 }
 
+// Outside of function!
 const $calculateTrigger = document.getElementById('calculate');
-
 $calculateTrigger.addEventListener('click', calculateAll);
+
+// Trigger for remove buttons
+document.querySelectorAll('.btn-remove').forEach(element => {
+  element.addEventListener('click', addProductRemoveListener);
+});
+
 
 // ITERATION 4
 
 function addProductRemoveListener($removeButton) {
-  // ...
+  let $rowToDelete = event.currentTarget.parentNode.parentNode.rowIndex;
+  document.getElementById("cart").deleteRow($rowToDelete);
 }
 
 // ITERATION 5
