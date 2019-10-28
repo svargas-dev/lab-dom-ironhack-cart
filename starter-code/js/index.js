eb4b43328c618e581eb442ed416e89f0c4b8e3c4
Sam Vargas <sam.vargas@gmx.com>
2019-10-28 13:42:02 +0000

    Iteration1 complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,18 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+  let $priceObj = document.querySelectorAll('.price > span');
+  let $quantityObj = document.querySelectorAll('.quantity > input');
+  let $subtotalObj = document.querySelectorAll('.subtotal > span');
+  
+  let $price = Number($priceObj[0].textContent);
+  let $quantity = + Number($quantityObj[0].valueAsNumber);
+  // console.log('price ' + $price);
+  // console.log('quanity ' + $quantity);
+  $subtotalObj[0].innerText = ($price * $quantity).toFixed(2);
 }
+
+
 
 function calculateAll() {
   // ITERATION 2
