77aeb9e18f1c8d6fcc6320e1ba339cd5ed63e710
Sam Vargas <sam.vargas@gmx.com>
2019-10-28 15:48:14 +0000

    Iteration5 done. Does not correctly calculate values or remove... yet

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -37,21 +37,56 @@ function calculateAll() {
 const $calculateTrigger = document.getElementById('calculate');
 $calculateTrigger.addEventListener('click', calculateAll);
 
-// Trigger for remove buttons
-document.querySelectorAll('.btn-remove').forEach(element => {
-  element.addEventListener('click', addProductRemoveListener);
-});
-
 
 // ITERATION 4
 
 function addProductRemoveListener($removeButton) {
   let $rowToDelete = event.currentTarget.parentNode.parentNode.rowIndex;
   document.getElementById("cart").deleteRow($rowToDelete);
+  calculateAll();
 }
+// Trigger for remove products
+document.querySelectorAll('.btn-remove').forEach(element => {
+  element.addEventListener('click', addProductRemoveListener);
+});
+
 
 // ITERATION 5
 
 function createProduct(event) {
-  // ...
+  let $newProduct = document.querySelector('.create-product input[type=text]').value;
+  let $newPrice = document.querySelector('.create-product input[type=number]').valueAsNumber;
+  let $newRow = document.querySelectorAll('#cart > tbody > tr').length;
+  // console.log($newProduct);
+  // console.log($newPrice);
+  // console.log($newRow);
+  let $table = document.querySelector('#cart > tbody')
+  let $row = $table.insertRow($newRow);
+  $row.innerHTML = ` 
+  <tr class="product">
+  <td class="name">
+    <span>${$newProduct}</span>
+  </td>
+
+  <td class="price">$<span>${$newPrice}</span></td>
+  
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  
+  <td class="subtotal">$<span>0</span></td>
+  
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+</tr>`;
+
+  // let c1 = row.insertCell(0);
+  // let c2 = row.insertCell(1);
+  // c1.innerHTML = "NEW CELL1";
+  // c2.innerHTML = "NEW CELL2";
 }
+// Trigger to add products
+document.querySelectorAll('#create').forEach(element => {
+  element.addEventListener('click', createProduct);
+});
